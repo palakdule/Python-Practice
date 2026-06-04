034079f2298166d2072df990bb67ff8859d86af4
Palak Dule <palakdule30@gmail.com>
2026-06-04 23:23:59 +0530

    program demonstrating assignment operators

---
+++ Assignment_op.py
@@ -0,0 +1,19 @@
+num = 10
+num /= 5
+print("num :", num)
+
+num = 10
+num *= 5
+print("num :", num)
+
+num = 10
+num += 5
+print("num :", num)
+
+num = 10
+num -= 5
+print("num :", num)
+
+num = 10
+num **= 5 #**=power
+print("num :", num)
