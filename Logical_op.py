7e5bb7481071c59fb8ab0278738cf80107d4c693
Palak Dule <palakdule30@gmail.com>
2026-06-04 23:25:36 +0530

    program demonstrating logical operators

---
+++ Logical_op.py
@@ -0,0 +1,11 @@
+a = 50
+b = 30
+
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = True
+print("AND operator:",val1 and val1) #True only when both value is true
+
+print("OR operator:", (a==b) or (a>b)) #True when any one value is True
