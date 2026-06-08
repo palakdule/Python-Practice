8136915932298019a3b49151839ebf109808ced3
Palak Dule <palakdule30@gmail.com>
2026-06-08 22:39:15 +0530

    Add Python string indexing and slicing examples

---
+++ Strings&conditional statements/indexing&slicing.py
@@ -0,0 +1,18 @@
+#indexing
+str = "Palak Dule"
+ch = str[0]
+print(ch)
+
+#slicing
+str = "Palak Dule" 
+print(str[0:3])
+
+str = "Palak Dule"
+print(str[6:len(str)])
+print(str[:5]) #[0:4]
+print(str[6:]) #[5:len(str)]
+
+#slicing(Negative index)
+str = "Apple"
+print(str[-3:-1])
+print(str[-5:-2])
