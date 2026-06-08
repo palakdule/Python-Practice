6a7bc98b56bf93ee79f468bffc3b7f7d45c06726
Palak Dule <palakdule30@gmail.com>
2026-06-08 22:13:34 +0530

    Create string.py with basic string operations
    Added a Python script demonstrating string operations, including concatenation and length calculation.

---
+++ Strings&conditional statements/string.py
@@ -0,0 +1,28 @@
+#String
+str1 = "This is a string."
+str2 = 'python'
+str3 = """this is a string"""
+
+#escape sequence character
+str1 = "This is a string. \nwe are creating it in python."
+print(str1)
+
+#Basic operation 
+#concatenation 
+str1 = "Palak"
+str2 = "dule"
+final_str = str1+str2
+print(final_str)
+
+#length of string 
+str1 = "Palak"
+len1 = len(str1)
+print(len1)
+
+str2 = "dule"
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " + str2
+print(final_str)
+print(len(final_str))
