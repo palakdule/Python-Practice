ca1ffabca5476e14660366c8ac4854bbe592b658
Palak Dule <palakdule30@gmail.com>
2026-06-08 23:03:50 +0530

    Add some examples of string functions

---
+++ Strings&conditional statements/Functions.py
@@ -0,0 +1,23 @@
+#str.endswith("on")
+str  = "I am studyng python"
+print(str.endswith("on")) #return true if string ends with substr
+
+#str.capitalize() = Capitalize 1st char
+str  = "i am studyng python"
+str = str.capitalize() #modify string
+print(str.capitalize()) #makes new string with changes
+print(str)
+
+#str.replace(old, new)
+str  = "I am studyng python"
+print(str.replace("o", "e"))
+print(str.replace("python", "Saap"))
+
+#str.find(word)
+str  = "I am studyng python"
+print(str.find("o"))
+
+
+#str.count()
+str  = "My name is palak and I am learning python"
+print(str.count("a"))
