66c5c1e9555642170dfaa4740acbb1987f3a544e
Palak Dule <palakdule30@gmail.com>
2026-06-16 21:32:08 +0530

    Add range function examples in range_ex.py
    Add examples demonstrating the use of the range function in Python.

---
+++ Range/range_ex.py
@@ -0,0 +1,20 @@
+#example 1 : range(stop))
+for i in range(10):
+    print(i)
+
+#example 2 : range(start, stop)
+for i in range(2, 10):
+    print(i)
+
+#example 3 : range(start, stop, step)
+for i in range(2, 10, 2):
+    print(i)
+
+#even number
+for i in range(2, 101, 2):
+    print(i)
+
+#odd numbers
+for i in range(1, 101, 2):
+    print(i)
+  
