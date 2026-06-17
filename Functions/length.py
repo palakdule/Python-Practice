33feafa34ca23cfa068cfdf73bf4bb7e1b4c3870
Palak Dule <palakdule30@gmail.com>
2026-06-18 00:09:25 +0530

    program to print the length of a list

---
+++ Functions/length.py
@@ -0,0 +1,8 @@
+cities = ["delhi", "gurgaon", "noida", "pune", "mumbai", "chennai"]
+fruits = ["mango", "banana", "apple", "grape"]
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(fruits)
+
