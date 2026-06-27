7f39a77ef1aeedba2e2f5e81465082e1640ab040
Palak Dule <palakdule30@gmail.com>
2026-06-27 23:44:22 +0530

    Implement file input/output operations in Python
    This script demonstrates various file operations in Python, including reading, writing, and deleting files using different modes and the 'with' syntax.

---
+++ File Input output/input_output.py
@@ -0,0 +1,65 @@
+#open file
+f = open("demo.txt", "r")
+
+#operation
+f = open("demo.txt", "r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
+
+#reading number of line
+f = open("demo.txt", "r")
+
+data = f.read(5)
+print(data)
+
+f.close()
+
+
+#readline()
+f = open("demo.txt", "r")
+
+line1 = f.readline()
+print(line1)
+
+f.close()
+
+
+#writing to a file
+f = open("demo.txt", "w")
+f.write("I want to learn python")
+f.close()
+
+
+#r+ mode
+f = open("demo.txt", "r+")
+f.write("abc")
+print(f.read())
+f.close()
+
+
+#w+ mode
+f = open("demo.txt", "r+")
+print(f.read())
+f.write("abc")
+f.close()
+
+#a+ mode
+f = open("demo.txt", "a+")
+print(f.read())
+f.write("abc")
+f.close()
+
+#with Syntax
+with open("demo.txt", "r")as f:
+    data = f.read()
+    print(data)
+
+with open("demo.txt", "w")as f:
+    f.write("new data")
+
+
+#Deleting a file
+import os
+os.remove("sample.txt")
