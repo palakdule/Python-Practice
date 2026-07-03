6edd787f030e17676b316fbdb330cd68b73ae725
Palak Dule <palakdule30@gmail.com>
2026-07-03 23:10:53 +0530

    Update oops.py

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -121,3 +121,60 @@ class Person:
 
 p1 = Person()
 print(p1.welcome())
+
+#Inheritance
+class Car:
+    @staticmethod
+    def start():
+        print("car started..")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class ToyotaCar(Car):
+    def __init__(self, name):
+        self.name = name
+
+car1 = ToyotaCar("fortuner")
+car2 = ToyotaCar("prius")
+
+print(car1.name)
+
+
+#multi-level inheritance
+class Car:
+    @staticmethod
+    def start():
+        print("car started..")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class ToyotaCar(Car):
+    def __init__(self, brand):
+        self.brand = brand
+
+class Fortuner(ToyotaCar):
+    def __init__(self, type):
+        self.type = type
+
+car1 = Fortuner("diesel")
+car1.start()
+
+
+#multiple inheritance
+class A:
+    varA = "welcome to class A"
+
+class B:
+    varB = "welcome to class B"
+
+class C(A, B):
+    varC = "welcome to class C"
+
+c1 = C()
+print(c1.varC)
+print(c1.varB)
+print(c1.varA)
