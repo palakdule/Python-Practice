ea49bafa251da6cf2388c3d5da568f77f06cc53e
Palak Dule <palakdule30@gmail.com>
2026-07-03 20:31:49 +0530

    Update oops.py

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -85,3 +85,39 @@ s1 = Student("Palak", 97)
 s1.welcome()
 print(s1.get_marks())
 
+
+#del keyword
+class Student:
+    def __init__(self, name):
+       self.name = name
+
+s1 = Student("Palak")
+print(s1)
+del s1
+ 
+print(s1)
+
+
+#private attribute and method
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.acc_pass = acc_pass
+
+acc1 = Account("12345", "abcde")
+
+print(acc1.acc_no)
+
+
+#example
+class Person:
+    __name = "anonymous"
+
+    def __hello(self):
+        print("hello person!")
+
+    def welcome(self):
+       self.__hello()
+
+p1 = Person()
+print(p1.welcome())
